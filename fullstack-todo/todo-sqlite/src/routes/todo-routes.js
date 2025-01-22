8c0ddbd5288b3e925461671eed57d9991c22356f
sueson <sueson@Mac.mynet>
2025-01-22 18:31:09 +0000

    updated

--- fullstack-todo/todo-sqlite/src/routes/todo-routes.js
+++ fullstack-todo/todo-sqlite/src/routes/todo-routes.js
@@ -27,13 +27,39 @@ router.get('/', (req, res) => {
 });
 
 // Create a todo...
-router.post('/', (req, res) => {})
+router.post('/', (req, res) => {
+    const { task } = req.body;
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`);
+
+    const result = insertTodo.run(req.userId, task);
+    res.json({ id: result.lastInsertRowid, task, completed: 0 });
+})
 
 // Update a todo...
-router.put('/:id', (req, res) => {})
+router.put('/:id', (req, res) => {
+    const { completed } = req.body;
+    // for accessing id req.params is used...
+    const { id } = req.params;
+    // for query purpose req.query is used...
+    // const{ page } = req.query;
+
+    const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`)
+    updatedTodo.run(completed, id);
+
+    res.json({ message: 'Todo completed' });
+})
 
 // Delete a todo...
-router.delete('/:id', (req, res) => {})
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const userId = req.userId;
+
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
+    deleteTodo.run(id, userId);
+
+    // immediate disappear from client once delete is done, without send any response only refresh the browser to see the changes... 
+    res.send({ message: "Todo deleted" });
+})
 
 
 export default router;
